a67367d05d149f40d2ea11939a5e0f69ee260818
Anuj Ravindra Bhagat <aanujbhagat2828@gmail.com>
2024-01-15 23:18:31 +0000

    feat/ made a util to handle async tasks

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@
     // "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
 
 require(`dotenv`).config({path: './env'})
+import { application } from "express";
 import connectDB from "./db/connectDB.js";
 
-dotenv.config({
-    path:'./env'
-})
+// dotenv.config({
+//     path:'./env'
+// })
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.log("MONGO db Connection failed !!! ", err)
+});
