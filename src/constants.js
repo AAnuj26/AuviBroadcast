decf8b1b690ade557c7b76f71db8b0a320d21bd9
AAnuj26 <aanujbhagat2828@gmail.com>
2024-01-16 13:34:50 +0530

    refact

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "videoStreamingAppDB"
+export const DB_NAME = "videoStreamingAppDB";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import dotenv from "dotenv"
-    // "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
+// "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
 
-require(`dotenv`).config({path: './env'})
+require(`dotenv`).config({ path: "./env" });
 import { application } from "express";
 import connectDB from "./db/connectDB.js";
 
@@ -9,10 +9,12 @@ import connectDB from "./db/connectDB.js";
 //     path:'./env'
 // })
 
-connectDB().then(()=>{
+connectDB()
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port: ${process.env.PORT}`)
-    })
-}).catch((err) => {
-    console.log("MONGO db Connection failed !!! ", err)
-});
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db Connection failed !!! ", err);
+  });
