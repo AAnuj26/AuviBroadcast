10f55539aa067026d553d5702df3dc46c806ac59
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-02-04 20:24:14 +0530

    fix / user.controller.js , user.model.js, cloudinary.js and asynchandler

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
-    userName: {
+    username: {
       type: String,
       required: true,
       unique: true,
@@ -66,7 +66,7 @@ userSchema.methods.generateAccessToken = function () {
     {
       _id: this._id,
       email: this.email,
-      userName: this.username,
+      username: this.username,
       fullName: this.fullname,
     },
     process.env.ACCESS_TOKEN_SECRET,
