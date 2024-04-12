1bd2fb953d25341f11305e3df21c949de500d8da
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-04-12 16:10:11 +0530

    Feat / added sass and made first module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <>
-      <h1>Auvi Broadcast</h1>
+      <Navbar />
     </>
   );
 }
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import styles from "./navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className={styles.Navbar}>
+        <h1>Auvi Broadcast</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
