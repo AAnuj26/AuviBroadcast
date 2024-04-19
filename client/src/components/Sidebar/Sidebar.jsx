e5be3da423139d0cb857bcbd98bed6032079176b
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-04-19 13:57:02 +0530

    Refact / video listing page in client Feat / getVideoComment function in comment.controller.js

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,112 +1,14 @@
 import Styles from "./sidebar.module.scss";
 import menu from "../../assets/menu.svg";
 import logo from "../../assets/auvilogo.svg";
-import divkeSVG from "../../assets/microphone.svg";
-import ClockSVG from "../../assets/user.svg";
+import user from "../../assets/user.svg";
+import home from "../../assets/house.svg";
 import "../../index.scss";
 
 const Main = () => {
   return (
     <>
       <aside className={Styles.Sidebar}>
-        {/* <main>
-          <div>
-            <button>Home</button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <hr />
-          <div>
-            <button>You</button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <hr />
-          <div>
-            <button>Subscriptions</button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button></button>
-          </div>
-          <div>
-            <button>Show 36 more</button>
-          </div>
-          <hr />
-          <div>
-            <button>Settings</button>
-          </div>
-          <div>
-            <button>Report History</button>
-          </div>
-          <div>
-            <button>Help</button>
-          </div>
-          <div>
-            <button>Send Feedback</button>
-          </div>
-          <hr />
-          <footer>
-            <ul>
-              <li>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <a href="#">about</a>
-                <p>
-                  <a href="#">C 2024 google llc</a>
-                </p>
-              </li>
-            </ul>
-          </footer>
-        </main> */}
         <header className={Styles.firstDiv}>
           <button>
             <img src={menu} alt="menu" />
@@ -121,82 +23,184 @@ const Main = () => {
         <div>
           <main className={Styles.SideBarMain}>
             <div>
-              <button>Hello</button>
+              <button>
+                <div>
+                  <img src={home} alt="user" />
+                </div>
+                <div>
+                  <span>Home</span>
+                </div>
+              </button>
             </div>
             <div>
-              <button>Hello</button>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>{" "}
+            <div>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
             </div>
             <div>
-              <button>Hello</button>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
             </div>
             <div>
-              <button>Hello</button>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
             </div>
             <div>
-              <button>Hello</button>
-            </div>{" "}
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>
             <div>
-              <button>Hello</button>
-            </div>{" "}
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>
             <div>
-              <button>Hello</button>
-            </div>{" "}
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>
             <div>
-              <button>Hello</button>
-            </div>{" "}
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
+            </div>
             <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
-            </div>{" "}
-            <div>
-              <button>Hello</button>
+              <button>
+                <div>
+                  <img src={user} alt="user" />
+                </div>
+                <div>
+                  <span>Hello</span>
+                </div>
+              </button>
             </div>
           </main>
           <footer className={Styles.SideBarFooter}>
@@ -208,6 +212,8 @@ const Main = () => {
               <a href="#">About</a>
               <a href="#">About</a>
               <a href="#">About</a>
+              <a href="#">About</a>
+              <a href="#">About</a>
             </div>
             <div>
               <a href="#">Privacy</a>
@@ -216,23 +222,13 @@ const Main = () => {
               <a href="#">Privacy</a>
               <a href="#">Privacy</a>
               <a href="#">Privacy</a>
-            </div>
-            <div>
-              <a href="#">Privacy</a>
-              <a href="#">Privacy</a>
-              <a href="#">Privacy</a>
-              <a href="#">Privacy</a>
-              <a href="#">Privacy</a>
-              <a href="#">Privacy</a>
-            </div>{" "}
-            <div>
-              <a href="#">Privacy</a>
               <a href="#">Privacy</a>
               <a href="#">Privacy</a>
               <a href="#">Privacy</a>
               <a href="#">Privacy</a>
               <a href="#">Privacy</a>
             </div>
+
             <p>C 2024 Google LLC</p>
           </footer>
         </div>
---
+++ server/src/controllers/comment.controller.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model";
+import { Video } from "../models/video.model";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiError";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+  if (!videoId) {
+    throw new ApiError(400, "Video ID is required");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    await Comment.deleteMany({ videoId: videoId });
+    throw new ApiError(
+      400,
+      "Video not found. All associated comments have been deleted"
+    );
+  }
+  const commentsAggregate = Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        owner: {
+          $first: "$owner",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes,likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        likesCount: 1,
+        owner: {
+          username: 1,
+          fullName: 1,
+          "avatar.url": 1,
+        },
+        isLiked: 1,
+      },
+    },
+  ]);
+
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+  const comments = await Comment.aggregatePaginate(commentsAggregate, options);
+
+  if (!comments || comments.length === 0) {
+    return res.status(404).json(new ApiResponse(404, {}, "No comments found"));
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments obtained successfully"));
+});
