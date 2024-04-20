724b24585d9892a1285298271dcd98a0b3c6ce33
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-04-21 04:02:41 +0530

    Feat / Added comment router in app,js Refact / removed small bugs importing and exporting the functions

--- server/src/controllers/comment.controller.js
+++ server/src/controllers/comment.controller.js
@@ -1,22 +1,24 @@
 import mongoose from "mongoose";
-import { Comment } from "../models/comment.model";
-import { Video } from "../models/video.model";
-import { asyncHandler } from "../utils/asyncHandler";
-import { ApiError } from "../utils/ApiError";
-import { ApiResponse } from "../utils/ApiError";
+import { Comment } from "../models/comment.model.js";
+import { Video } from "../models/video.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
   if (!videoId) {
-    throw new ApiError(400, "Video ID is required");
+    throw new ApiError(400, "videoId is required!!");
   }
   const video = await Video.findById(videoId);
   if (!video) {
-    await Comment.deleteMany({ videoId: videoId });
+    // If the video doesn't exist, delete all comments associated with the video ID
+    await Comment.deleteMany({ video: videoId });
     throw new ApiError(
       400,
-      "Video not found. All associated comments have been deleted"
+      "There is no such Video. All associated comments have been deleted."
     );
   }
   const commentsAggregate = Comment.aggregate([
@@ -34,6 +36,14 @@ const getVideoComments = asyncHandler(async (req, res) => {
       },
     },
     {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+    {
       $addFields: {
         likesCount: {
           $size: "$likes",
@@ -43,7 +53,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
         },
         isLiked: {
           $cond: {
-            if: { $in: [req.user?._id, "$likes,likedBy"] },
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
             then: true,
             else: false,
           },
@@ -72,11 +82,19 @@ const getVideoComments = asyncHandler(async (req, res) => {
   const comments = await Comment.aggregatePaginate(commentsAggregate, options);
 
   if (!comments || comments.length === 0) {
-    return res.status(404).json(new ApiResponse(404, {}, "No comments found"));
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "No commments in this video!!"));
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, comments, "Comments obtained successfully"));
+    .json(
+      new ApiResponse(
+        200,
+        comments,
+        "Comments of the video fetched Successfully"
+      )
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -113,4 +131,103 @@ const addComment = asyncHandler(async (req, res) => {
   }
 });
 
-export { getVideoComments, addComment };
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { commentContent } = req.body;
+  if (!commentId) {
+    throw new ApiError(400, "Comment ID is required");
+  }
+  try {
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      throw new ApiError(404, "Comment not found");
+    }
+    const videoId = new mongoose.Types.ObjectId(comment.video);
+    const video = await Video.findById(videoId);
+    if (!video) {
+      await Comment.deleteMany({ video: videoId });
+      return res
+        .status(404)
+        .json(new ApiResponse(404, {}, "Comment doesn't exist"));
+    }
+    if (
+      video.owner.toString() !== req.user?._id.toString() &&
+      !video.isPublished
+    ) {
+      throw new ApiError(403, "Video doesn't exist");
+    }
+    if (comment.owner.toString() !== req.user?._id.toString()) {
+      throw new ApiError(300, "Unauthorized Access");
+    }
+    if (!commentContent) {
+      throw new ApiError(400, "commentContent is required!!");
+    }
+    const UpdatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        $set: {
+          content: commentContent,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (!UpdatedComment) {
+      throw new ApiError(500, "Unable to update the comment");
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, UpdatedComment, "Comment Updated Successfully")
+      );
+  } catch (error) {
+    throw new ApiError(500, e?.message || "Unable to updfate the comment");
+  }
+});
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+  const { commentId } = req.params;
+
+  if (!commentId) {
+    throw new ApiError(400, "commentId is Required!!");
+  }
+  try {
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      throw new ApiError(404, "comment not found");
+    }
+    //video is published or not
+    const videoId = new mongoose.Types.ObjectId(comment.video);
+    const video = await Video.findById(videoId);
+    if (!video) {
+      // If the video doesn't exist, delete all comments associated with the video ID
+      await Comment.deleteMany({ video: videoId });
+      throw new ApiError(
+        400,
+        "There is no such Video. All associated comments have been deleted."
+      );
+    }
+    if (
+      video.owner.toString() !== req.user?._id.toString() &&
+      !video.isPublished
+    ) {
+      throw new ApiError(300, "Video doesn't exists");
+    }
+    if (comment.owner.toString() !== req.user?._id.toString()) {
+      throw new ApiError(300, "Unauthorized Access");
+    }
+    const DeletedComment = await Comment.findByIdAndDelete(commentId);
+    if (!DeletedComment) {
+      throw new ApiError(500, "Unable to delete the comment");
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Comment deleted Successfully"));
+  } catch (e) {
+    throw new ApiError(500, e?.message || "Unable to delete the comment");
+  }
+});
+export { getVideoComments, addComment, updateComment, deleteComment };
--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
 import {
   getVideoComments,
   addComment,
-} from "../controllers/comment.controller";
+  updateComment,
+  deleteComment,
+} from "../controllers/comment.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/:videoId").get(getVideoComments).post(addComment);
+router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+
+export default router;
