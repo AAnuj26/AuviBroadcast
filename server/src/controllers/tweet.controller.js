6ed8c8e37fa7c91ab96c848348ba95e59edfbaf5
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-05-01 21:43:21 +0530

    Add getUserTweets functionality to tweet.controller.js and tweet.routes.js

--- server/src/controllers/tweet.controller.js
+++ server/src/controllers/tweet.controller.js
@@ -2,6 +2,7 @@ import { Tweet } from "../models/tweet.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose, { isValidObjectId } from "mongoose";
 
 const createTweet = asyncHandler(async (req, res) => {
   const { tweetContent } = req.body;
@@ -24,4 +25,44 @@ const createTweet = asyncHandler(async (req, res) => {
   }
 });
 
-export { createTweet };
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  if (!userId) {
+    throw new ApiError(400, "userId is Required!!!");
+  }
+  try {
+    const tweet = await Tweet.aggregate([
+      {
+        $match: {
+          owner: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $group: {
+          _id: "owner",
+          tweets: { $push: "$content" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          tweets: 1,
+        },
+      },
+    ]);
+    if (!tweet || tweet.length === 0) {
+      return res
+        .status(200)
+        .json(new ApiResponse(200, [], "User have no tweets"));
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, tweet, "Tweet for the user fetched successfully!")
+      );
+  } catch (e) {
+    throw new ApiError(500, e?.message || "Unable to fetch tweets");
+  }
+});
+
+export { createTweet, getUserTweets };
--- server/src/routes/tweet.routes.js
+++ server/src/routes/tweet.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, getUserTweets } from "../controllers/tweet.controller.js";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/").post(createTweet);
+router.route("/user/:userId").get(getUserTweets);
 
 export default router;
