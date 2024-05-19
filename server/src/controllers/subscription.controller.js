d17c1f938e3dbcb14b6077caf81df7560fbddb05
AAnuj26 <bhagatanuj2828@gmail.com>
2024-05-19 17:21:08 +0530

    Added toggleSubscription functionality in subscription controller to subscribe or unsubscribe the user, then added routes to access this functionality into subscription routes

--- server/src/controllers/subscription.controller.js
+++ server/src/controllers/subscription.controller.js
@@ -1,1 +1,53 @@
+import mongoose from "mongoose";
 import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+  // TODO: toggle subscription
+  if (!channelId) {
+    throw new ApiError(400, "channelId is Required!!");
+  }
+  const userId = req.user?._id;
+  const credential = { subscriber: userId, channel: channelId };
+  try {
+    const subscribed = await Subscription.findOne(credential);
+    if (!subscribed) {
+      //not subscribed :- delete the existing one
+      const newSubscription = await Subscription.create(credential);
+      if (!newSubscription) {
+        throw new ApiError(500, "Unable to Subscribe channel");
+      }
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            newSubscription,
+            "Channel Subscribed Successfully!!"
+          )
+        );
+    } else {
+      //subscribed :-delete the subscription
+      const deletedSubscription = await Subscription.deleteOne(credential);
+      if (!deletedSubscription) {
+        throw new ApiError(500, "Unable to Unsubscribe channel");
+      }
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            deletedSubscription,
+            "Channel Unsubscribed Successfully!!"
+          )
+        );
+    }
+  } catch (e) {
+    throw new ApiError(500, e?.message || "Unable to toggle subscription");
+  }
+});
+
+export { toggleSubscription };
--- server/src/routes/subscription.routes.js
+++ server/src/routes/subscription.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 
+import { toggleSubscription } from "../controllers/subscription.controller.js";
+
 import { verifyJWT } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 router.use(verifyJWT);
+
+router.route("/c/:channelId").post(toggleSubscription);
 
 export default router;
