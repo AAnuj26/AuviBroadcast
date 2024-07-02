93e4f1e615c209bffbb8a93a901bf3cf441110b1
AAnuj26 <bhagatanuj2828@gmail.com>
2024-07-02 17:00:38 +0530

    refact

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,78 +1,8 @@
-// import menu from "./assets/menu.svg";
 import Navbar from "./components/Navbar/Navbar";
-// import Sidebar from "./components/Sidebar/Sidebar";
-// import VideoCard from "./components/VideoCard/VideoCard";
-// import VideoListingPage from "./pages/VideoListingPage/VideoListingPage";
 function App() {
-  // const se = { width };
   return (
     <>
-      {/* <Navbar />
-      <h2>Hello</h2> */}
-      {/* <nav style={{ height: "40px", width: "100%", display: "flex" }}>
-        <img src={menu} alt="menu" />
-      </nav> */}
-      {/* <Sidebar /> */}
-      {/* <div
-        style={{
-          height: "100vh",
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          // padding: "5px",
-          // overflow: "none",
-          // margin: "1rem",
-        }}
-      >
-        <div
-          style={{
-            height: "300px",
-            width: "49%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: ".5rem",
-            // backgroundColor: "red",
-          }}
-        >
-          <VideoCard />
-        </div>
-        <div
-          style={{
-            height: "300px",
-            width: "49%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "0.5rem",
-
-            // backgroundColor: "red",
-          }}
-        >
-          <VideoCard />
-        </div>
-      </div> */}
-      {/* <VideoCard /> */}
-      {/* <div
-        style={{
-          height: "40px",
-          backgroundColor: "red",
-          width: "100%",
-          // display: "block",
-        }}
-      ></div>
-      <div
-        style={{ height: "100dvh", width: "100%", backgroundColor: "green" }}
-      >
-        <VideoListingPage />
-      </div> */}
-      {/* <VideoCard /> */}
       <Navbar />
-      {/* <div>
-        {/* <Sidebar /> */}
-      {/* <VideoListingPage /> */}
-      {/* </div> */}
     </>
   );
 }
